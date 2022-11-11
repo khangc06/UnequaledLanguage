4d20fd2ea014f8b14f4612c5da4a18970254c172
khangc06 <114877729+khangc06@users.noreply.github.com>
2022-11-11 14:08:46 -0800

    Update Grocerystore.py

--- Grocerystore.py
+++ Grocerystore.py
@@ -1,8 +1,8 @@
 class GroceryStore:
     def main():
         #Menu
-        products=["milk", "ice cream", "apple", "banana", "cherry", "meat", "fish", "juice", "bread", "rice"]
-        price   =["3.89", "4.99", "1.5", "2.99", "2.89", "4.89", "4.99", "3.5", "2.5", "1.5"]
+        products=["","milk", "ice cream", "apple", "banana", "cherry", "meat", "fish", "juice", "bread", "rice"]
+        price   =["","3.89", "4.99", "1.5", "2.99", "2.89", "4.89", "4.99", "3.5", "2.5", "1.5"]
         print("-Products-\n1.Milk-3.89$\n2.Ice cream-4.99$\n3.Apple=1.5$\n4.Banana-2.99$\n5.Cherry-2.89$\n6.Meat-4.89$\n7.Fish-4.99$\n8.Juice-3.5$\n9.Bread-2.5$\n10.Rice-1.5$")
         #Asking first products
         ask=int(input("Which products do you want to buy? "))
@@ -15,10 +15,11 @@ class GroceryStore:
         if (ask1 == "Yes"):
             while (ask != 0):
                 ask=int(input("Which products do you want to buy? - Press 0 to check out "))
-                print("You choose " + products[ask] + ", " + "the price is " + price[ask] + "$")
-                total1= price[ask]
-                total= float(total) + float(total1)
-                print("Total price: " + str(total)+"$")
+                if (ask !=0):
+                    print("You choose " + products[ask] + ", " + "the price is " + price[ask] + "$")
+                    totaln= price[ask]
+                    total= float(total) + float(totaln)
+                    print("Total price: " + str(total)+"$")
 
         #Total cost
         print("Your total cost: " + str(total) + "$")
